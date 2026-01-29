a26de6cf9ae20082cea126bf341b863f343d2846
JM Bell <justinmbell1997@gmail.com>
2026-01-29 08:44:36 -0600

    Edited photo web server to update info on page
    if new images are found then new information will display on the page

--- FinalProject/photo_web_server.py
+++ FinalProject/photo_web_server.py
@@ -6,17 +6,29 @@ from datetime import datetime, date
 
 main_dir = "/home/jmbell97/Documents/RaspberryPi/FinalProject/static/images"
 replacement = "/home/jmbell97/Documents/RaspberryPi/FinalProject"
+latest_file_replacement = "/home/jmbell97/Documents/RaspberryPi/FinalProject/static/images/Image"
 new_img_dir = "/home/jmbell97/Documents/RaspberryPi/FinalProject/static/images/new"
 extension = "*.jpg"
-jpg_files = list(Path(main_dir).glob(extension))
-latest_file = jpg_files[len(jpg_files)-1]
     
 def getLatestImage():
     # Get count of all images
     jpg_files = list(Path(main_dir).glob(extension))
-    latest_file = jpg_files[len(jpg_files)-1]
-    image_path = str(latest_file)
-    image_path = image_path.replace(replacement, "")
+    count = len(jpg_files) - 1
+    count_characters = len(str(count))
+    print(f"Latest image number: {count}")
+    latest_file = ''
+    for file in jpg_files:
+        filepath = str(file)
+        print(f"The file path is: {filepath}")
+        new_file = filepath.replace(latest_file_replacement, "")
+        print(f"The replacement is: {new_file}")
+        print(f"file number search: {new_file[0:count_characters]}")
+        if new_file[0:count_characters] == str(count):
+            latest_file = str(file)
+#     latest_file = jpg_files[len(jpg_files)-1]
+#     image_path = str(latest_file)
+    image_path = latest_file.replace(replacement, "")
+    print(f"The final image path: {image_path}")
     return image_path
 
 def getNewImageCount(): 
