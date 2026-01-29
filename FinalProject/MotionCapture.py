2785ea02f374fb50b518f81b492a3a30857fa187
JM Bell <justinmbell1997@gmail.com>
2026-01-29 11:27:39 -0600

    FinalProject - added PDF changes

--- FinalProject/MotionCapture.py
+++ FinalProject/MotionCapture.py
@@ -1,36 +1,26 @@
-# if motion is detected for 5 seconds on PIR then power on LED and wait 5 seconds then take a photo
-#save photo to a log file
-
 import os
 from gpiozero import LED, MotionSensor
 import time
 from picamzero import Camera
 import yagmail
 from pathlib import Path
-import shutil
 
-# Set up email
-password = ""
-with open("/home/jmbell97/Documents/temp.txt", "r") as f:
-    password = f.read().rstrip()
-yag = yagmail.SMTP("justinsraspberrypi71@gmail.com", password)
-
-
-# Create directory for images
-new_image_dir = "/home/jmbell97/Documents/RaspberryPi/FinalProject/static/images/new"
-if not os.path.exists(new_image_dir):
-    os.mkdir(new_image_dir)
-    
 
 # Set up image name by finding out how many images are currently in folder
-main_dir = "/home/jmbell97/Documents/RaspberryPi/FinalProject/static/images"
+main_dir = "/home/jmbell97/Documents/RaspberryPi/FinalProject/img"
+if not os.path.exists(main_dir):
+    os.mkdir(main_dir)
+new_image_file = "/home/jmbell97/Documents/RaspberryPi/FinalProject/photo_logs.txt"
 extension = "*.jpg"
 jpg_files = list(Path(main_dir).glob(extension))
-counter = len(jpg_files)
-image_name = "/Image"
+image_name = "/img_"
 extension = ".jpg"
 file = ""
 
+lastTimeMotionDetected = time.time()
+lastTimePhotoTaken = time.time()
+motionDetectDelay = 5.0
+takePhotoDelay = 30.0
 
 # set up hardware
 red_led = LED(17)
@@ -39,45 +29,90 @@ camera = Camera()
 camera.still_size = (1640, 1232)
 
 
+class MotionCapture(): 
+
+    def __init__(self):
+        """
+        constructor for MotionCapture class
+        
+        :param self: MotionCapture - The MotionCapture object
+        """
+        self.remove_txt_file()
+
+    def take_photo(file):
+        """
+        A method that takes a photo with the camera and saves it.
+        
+        :param file: String - the filepath that the photo is saved to.
+        """
+        camera.take_photo(file)
+        print("Photo Made. Saving photo to main folder.")
+        
+        
+    def write_to_txt_file(file):
+        """
+        A method that stores the filepath of a new image in
+        a log text file.
+        
+        :param file: String - filepath of the new image
+        """
+        if not os.path.exists(new_image_file):
+            with open(new_image_file, 'a') as opened_file:
+                opened_file.write(f"{file}\n")
+        print(f"File saved as: {file}")
+
+
+    def remove_txt_file(file):
+        """
+        A method that deletes the image storage text 
+        file if it does exist. 
+        
+        :param file: String - filepath for images
+        """
+        if os.path.exists(file):
+            os.remove(file)
+
+
+    def send_photo_by_email(file):
+        # Set up email
+        password = ""
+        with open("/home/jmbell97/Documents/temp.txt", "r") as f:
+            password = f.read().rstrip()
+        yag = yagmail.SMTP("justinsraspberrypi71@gmail.com", password)
+        time.sleep(2)
+        subject = "New Image"
+        contents = "Image: " + file
+        yag.send(to="justinmbell1997@gmail.com",
+                    subject=subject,
+                    contents=contents,
+                    attachments=file)
+        print("Email sent")
+        time.sleep(2)
+
+photo_taker = MotionCapture()
+
 try:
     while True:
         #created file string
-        file = new_image_dir + image_name
-        file += str(counter)
-        file += extension
+        now = time.time() 
+        image_name += str(now)
+        image_name += extension
+        file = main_dir + image_name
+        if (now - lastTimePhotoTaken > takePhotoDelay):
+            if (pir.motion_detected):
+                # turn LED on
+                red_led.on()
+                if (now - lastTimeMotionDetected > motionDetectDelay):
+                    # take photo
+                    print("LED on. Taking a photo.")
+                    photo_taker.take_photo(file)
+                    photo_taker.write_to_txt_file(file)
+                    lastTimeMotionDetected += now
 
-        if (pir.motion_detected):
-            # turn LED on and take photo
-            red_led.on()
-            print("LED on. Taking a photo.")
-            time.sleep(5)
-            camera.take_photo(file)
-            print("Photo Made. Saving photo to main folder.")
-            time.sleep(1)
-
-            # try-catch for copying new images into main directory
-            try:
-                shutil.copy(file, main_dir)
-                print(f"File {file} is now saved to directory: {main_dir}")
-                time.sleep(2)
-            except Exception as e:
-                print(f"An error occurred {e}")
-                time.sleep(2)
-
-            # send email with new photo attached
-            print("Sending Email Now.")
-            time.sleep(2)
-            subject = "Image " + str(counter)
-            contents = "This is Image Number: " + str(counter)
-            yag.send(to="justinmbell1997@gmail.com",
-                     subject=subject,
-                     contents=contents,
-                     attachments=file)
-            print("Email sent")
-            time.sleep(2)
-            counter += 1        
-        else:
-            red_led.off()
-            print("LED off")
+                    # send email with new photo attached
+                    print("Sending Email Now.")
+                    photo_taker.send_photo_by_email(image_name)    
+            else:
+                red_led.off()
 except KeyboardInterrupt:
     pass
--- FinalProject/photo_web_server.py
+++ FinalProject/photo_web_server.py
@@ -5,64 +5,72 @@ from pathlib import Path
 from datetime import datetime, date
 
 # file paths
-main_dir = "/home/jmbell97/Documents/RaspberryPi/FinalProject/static/images"
+main_dir = "/home/jmbell97/Documents/RaspberryPi/FinalProject/img"
 replacement = "/home/jmbell97/Documents/RaspberryPi/FinalProject"
-latest_file_replacement = "/home/jmbell97/Documents/RaspberryPi/FinalProject/static/images/Image"
+latest_file_replacement = "/home/jmbell97/Documents/RaspberryPi/FinalProject/static/img/img_"
 new_img_dir = "/home/jmbell97/Documents/RaspberryPi/FinalProject/static/images/new"
 extension = "*.jpg"
 
 
-def getLatestImage():
-    """
-    A method that gets the latest image in the iamge directory.
+# def getLatestImage():
+#     """
+#     A method that gets the latest image in the iamge directory.
 
-    :return the String of the file path of the latest image
-    """
-    # Get count of all images
-    jpg_files = list(Path(main_dir).glob(extension))
-    count = len(jpg_files) - 1
-    count_characters = len(str(count))
-    print(f"Latest image number: {count}")
-    latest_file = ''
-    for file in jpg_files:
-        filepath = str(file)
-        print(f"The file path is: {filepath}")
-        new_file = filepath.replace(latest_file_replacement, "")
-        print(f"The replacement is: {new_file}")
-        print(f"file number search: {new_file[0:count_characters]}")
-        if new_file[0:count_characters] == str(count):
-            latest_file = str(file)
-#     latest_file = jpg_files[len(jpg_files)-1]
-#     image_path = str(latest_file)
-    image_path = latest_file.replace(replacement, "")
-    print(f"The final image path: {image_path}")
-    return image_path
+#     :return the String of the file path of the latest image
+#     """
+#     # Get count of all images
+#     jpg_files = list(Path(main_dir).glob(extension))
+#     count = len(jpg_files) - 1
+#     count_characters = len(str(count))
+#     print(f"Latest image number: {count}")
+#     latest_file = ''
+#     for file in jpg_files:
+#         filepath = str(file)
+#         print(f"The file path is: {filepath}")
+#         new_file = filepath.replace(latest_file_replacement, "")
+#         print(f"The replacement is: {new_file}")
+#         print(f"file number search: {new_file[0:count_characters]}")
+#         if new_file[0:count_characters] == str(count):
+#             latest_file = str(file)
+# #     latest_file = jpg_files[len(jpg_files)-1]
+# #     image_path = str(latest_file)
+#     image_path = latest_file.replace(replacement, "")
+#     print(f"The final image path: {image_path}")
+#     return image_path
 
-def getNewImageCount(): 
-    """
-    A method that gets the number of new images taken since
-    the last refresh of the 'check-photos' page.
 
-    :return int of number of new images
+def getLatestImagePath(): 
+    """
+    A method that gets the latest image in the image directory.
+
+    :return String - filepath of latest image
     """
     # Get count of new images
-    new_images = list(Path(new_img_dir).glob(extension))
-    new_img_count = len(new_images)
-    return new_img_count
-    
-def clearNewImageFolder():
+    all_images = list(Path(main_dir).glob(extension))
+    all_images.sort()
+    all_images_count = len(all_images)
+    latest_image_path = all_images[all_images_count-1]
+    latest_image = str(latest_image_path)
+    return latest_image
+
+def getTextFileCount():
     """
-    A method that clears the 'new' folder that contains the 
-    new images that are taken before the refresh of the page.
+    A method that scans the photo storage text file for the 
+    number of files.
+
+    :return int - number of new images
     """
-    new_images = list(Path(new_img_dir).glob(extension))
-    for image in new_images:
-        try:
-            os.remove(image)
-            print(f"Removed path: {image}") 
-        except Exception as e:
-            print(f"An error occurred {e}")
-    
+    txt_filepath = "/home/jmbell97/Documents/RaspberryPi/FinalProject/photo_logs.txt"
+    counter = 0
+    if os.path.exists(txt_filepath):
+        with open(txt_filepath, 'r') as opened_file:
+            for line in opened_file:
+                print(line)
+                counter += 1
+        os.remove(txt_filepath)
+    return counter
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -78,10 +86,8 @@ def check_photos():
     A function that uses the image file functions to render the latest image
     and update the state of whether there are new images since the last refresh.
     """
-    # add check in this function to check to see if a new photo has been added to list
-    image_path = getLatestImage()
-    new_img_count = getNewImageCount()
-    clearNewImageFolder()
+    image_path = getLatestImagePath()
+    new_img_count = getTextFileCount()
     return render_template('index.html', image_path=image_path, new_img_count=new_img_count)
         
 app.run(host="0.0.0.0", port=8500)    
